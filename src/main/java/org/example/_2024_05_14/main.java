aeb5a66733560bc86b6b4f6822dcc49233f64337
dgali <dgalita197@gmail.com>
2024-05-15 12:41:56 +0200

    start

---
+++ src/main/java/org/example/_2024_05_14/main.java
@@ -0,0 +1,7 @@
+package org.example._2024_05_14;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/org/example/_2024_05_15/Test.java
@@ -0,0 +1,77 @@
+package org.example._2024_05_15;
+
+public class Test {
+    public static void main(String[] args) {
+
+    //примитивные типы данных
+     int intValue = 10;
+     double doubleValue =3.14;
+     boolean bolesnValue = true;
+     char charValue ='a';
+        byte byteValue = 1;
+        short shortValue = 500;
+        long longValue = 50000000;
+
+        System.out.println (intValue);
+        System.out.println (doubleValue);
+        System.out.println (bolesnValue);
+        System.out.println (charValue);
+        System.out.println (byteValue);
+        System.out.println (shortValue);
+        System.out.println (longValue);
+
+
+     //не примитивные типы данных
+        String stringValue ="hello world";
+        Integer integerValue = 20;
+        Double doubleObject = 2.71;
+        Boolean BooleanObject = false;
+        Character characterObject = 'b';
+
+        System.out.println (stringValue);
+        System.out.println (integerValue);
+        System.out.println (doubleObject);
+        System.out.println (BooleanObject);
+        System.out.println (characterObject);
+
+
+        int a = 168;
+        int b = 1500;
+
+        // сложение
+        int sum = a + b;
+        System.out.println(sum);
+
+        //вычетание
+        int difference = a - b;
+        System.out.println(difference);
+
+        // умножение
+        int product = a * b;
+        System.out.println(product);
+
+       // Деление
+       int division = a / b;
+       System.out.println(division);
+
+       // Остаток от деления
+       int remainder = a % b;
+       System.out.println(remainder);
+
+        // Инкремент (увеличение на 1)
+       a++;
+       System.out.println( + a);
+
+       // Декремент (уменьшение на 1)
+       b--;
+       System.out.println(+ b);
+
+
+
+
+
+
+
+
+    }
+}
